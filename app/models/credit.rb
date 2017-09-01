342ccac509533f8be4f56cce27b4d89654726a03
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-01 19:23:21 +0300

    Add credit validations

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,5 +1,10 @@
 class Credit < ApplicationRecord
   belongs_to :debtor
 
+  validates :rate, numericality: { only_integer: true }
+  validates :arrears_rate, numericality: { only_integer: true }
+  validates :payment_period_in_months, numericality: { only_integer: true }
+  validates :date, date: true
+
   monetize :amount_cents
 end
--- spec/models/credit_spec.rb
+++ spec/models/credit_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Credit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do 
+    it { is_expected.to validate_numericality_of(:rate).only_integer }
+    it { is_expected.to validate_numericality_of(:arrears_rate).only_integer }
+    it { is_expected.to validate_numericality_of(:payment_period_in_months).only_integer }
+    it { is_expected.to validate_numericality_of(:amount) }
+    it { is_expected.to belong_to(:debtor) }
+  end
 end
