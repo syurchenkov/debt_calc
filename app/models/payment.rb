de0e24791ed44ecda6342dcbc23bb054377ef5c4
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-03 21:44:44 +0300

    Add validation of number of payments per credit

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,14 @@ class Payment < ApplicationRecord
   belongs_to :credit
   validates :date, date: true
   monetize :amount_cents
+
+  before_save :validate_credit_payments_limit
+
+  private 
+
+  def validate_credit_payments_limit
+    return nil if credit.payments.size < credit.payment_period_in_months
+    errors.add(:credit, 'has maximum number of payments')
+    raise ActiveRecord::Rollback
+  end
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,4 +2,60 @@ require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
   it { is_expected.to belong_to(:credit) }
+
+
+  context 'when attempting to add more than the permitted number of payments to a credit' do 
+    let(:number_of_payments) { 4 }
+    let!(:credit) do 
+      create(:credit_with_payments,
+        payment_period_in_months: number_of_payments, 
+        payments_count: number_of_payments
+      )
+    end
+
+
+    context 'by creating new payment' do 
+      let(:payment) { build(:payment, credit_id: credit.id) }
+
+      it 'can not create a payment' do 
+        expect { payment.save }.not_to change{ Payment.count }
+      end
+    end
+
+    context 'by updating existing payment' do 
+      let!(:another_credit) { create(:credit) }
+      let(:payment) { create(:payment, credit: another_credit) }
+
+      it 'can not update a payment' do 
+        expect { payment.update(credit_id: credit.id) }.not_to change{ credit.payments.count }
+      end
+    end
+  end
+
+  context 'when attempting to add the permitted number of payments to a credit' do 
+    let(:number_of_payments) { 4 }
+    let!(:credit) do 
+      create(:credit_with_payments,
+        payment_period_in_months: number_of_payments, 
+        payments_count: number_of_payments - 1
+      )
+    end
+
+    context 'by creating new payment' do 
+      let(:payment) { build(:payment, credit_id: credit.id) }
+
+      it 'creates a payment' do 
+        expect { payment.save }.to change{ Payment.count }.by(1)
+      end
+    end
+
+    context 'by updating existing payment' do 
+      let!(:another_credit) { create(:credit) }
+      let(:payment) { create(:payment, credit: another_credit) }
+
+      it 'updates a payment' do 
+        expect { payment.update(credit_id: credit.id) }.to change{ credit.payments.count }.by(1)
+      end
+    end    
+  end 
 end
