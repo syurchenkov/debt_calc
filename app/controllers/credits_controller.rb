8b1d71b175d2429496121d9d6a9f82b4f24cd683
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-05 00:15:46 +0300

    Fix n + 1 queries in credit show action

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -64,7 +64,7 @@ class CreditsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_credit
-      @credit = Credit.find(params[:id])
+      @credit = Credit.includes(:debtor).includes(:payments).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
