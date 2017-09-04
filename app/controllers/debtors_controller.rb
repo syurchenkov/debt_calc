042f02a44e18178bcd47d6d1197d8f52a10803c3
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-05 00:13:51 +0300

    Fix n + 1 queries in debtor show action

--- app/controllers/debtors_controller.rb
+++ app/controllers/debtors_controller.rb
@@ -64,7 +64,7 @@ class DebtorsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_debtor
-      @debtor = Debtor.find(params[:id])
+      @debtor = Debtor.includes(:credits).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
