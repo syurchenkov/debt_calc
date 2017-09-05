4aba00d98058ec0dd874cfe219e282591872663e
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-05 10:50:46 +0300

    Fix payment factory

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -3,17 +3,17 @@ FactoryGirl.define do
     credit { create(:credit) }
     amount { credit.regular_payment }
     date "2017-09-01"
-    is_arrear false
+    is_arrear '0'
     is_early_repayment false
 
     trait :arrear do 
-      is_arrear true
+      is_arrear '1'
       amount { credit.arrear_payment }
     end
 
     trait :early do 
       is_early_repayment true
-      amount { ((is_arrear) ? (credit.arrear_payment):(credit.regular_payment)) + credit.early_repayment }
+      amount { ((is_arrear == '1') ? (credit.arrear_payment):(credit.regular_payment)) + credit.early_repayment }
     end
   end
 end
