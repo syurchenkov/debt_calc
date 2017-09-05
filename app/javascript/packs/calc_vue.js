54dc54c5cdd7e1ec00e70cc31670b1923905b132
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-05 13:20:58 +0300

    Add basic vue js appon root page only

---
+++ app/javascript/packs/calc_vue.js
@@ -0,0 +1,23 @@
+/* eslint no-console: 0 */
+// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> and
+// <%= stylesheet_pack_tag 'hello_vue' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.
+
+import Vue from 'vue/dist/vue.esm'
+import App from './app.vue'
+
+
+import TurbolinksAdapter from 'vue-turbolinks'
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  var element = document.getElementById("calculator")
+  if (element != null) {
+    var vueapp = new Vue({
+      el: element,
+      template: '<App/>',
+      components: { App },
+    });
+  }
+});
