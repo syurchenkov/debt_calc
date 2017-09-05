53c4c01c553b69917f77fef20f15b78a2a4ef11c
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-09-05 16:32:39 +0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+debtors = Array.new(4) { |i| Debtor.create!(name: "Debtor#{i + 1}") }
+
+credits = Array.new(3) do |i|
+  Credit.create!(
+    debtor: debtors[i], 
+    amount: Money.from_amount(1_000_000),
+    rate: 30,
+    arrears_rate: 50,
+    payment_period_in_months: 6,
+    date: DateTime.now.to_date
+  )
+end
+
+# payments of first credit
+(1..6).each do 
+  Payment.create!(
+    credit: credits[0],
+    amount: Money.from_amount(191_666.67),
+    is_arrear: '0',
+    is_early_repayment: false,
+    date: DateTime.now.to_date
+  )
+end
+
+# payments of second credit
+(1..3).each do 
+  Payment.create!(
+    credit: credits[1],
+    amount: Money.from_amount(191_666.67),
+    is_arrear: '0',
+    is_early_repayment: false,
+    date: DateTime.now.to_date
+  )
+end
+
+Payment.create!(
+  credit: credits[1],
+  amount: Money.from_amount(525_000.01),
+  is_arrear: '0',
+  is_early_repayment: true,
+  date: DateTime.now.to_date
+)
+
+# payments of third credit
+(1..2).each do 
+  Payment.create!(
+    credit: credits[2],
+    amount: Money.from_amount(191_666.67),
+    is_arrear: '0',
+    is_early_repayment: false,
+    date: DateTime.now.to_date
+  )
+end
+
+(1..4).each do 
+  Payment.create!(
+    credit: credits[2],
+    amount: Money.from_amount(208_333.34),
+    is_arrear: '1',
+    is_early_repayment: false,
+    date: DateTime.now.to_date
+  )
+end
